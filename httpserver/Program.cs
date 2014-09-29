f47884b88041943d8c4a78d673f64d3b2cd1a96b
MadsMorsing <madsmorsing@gmail.com>
2014-09-29 13:55:08 +0200

    Klar til opgave

--- httpserver/Program.cs
+++ httpserver/Program.cs
@@ -9,16 +9,10 @@ namespace httpserver
     class Program
     {
 
-        /// <summary>
-        /// Main metoden for programmet
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello http server");
-            //Test Fra Johan
-
-            // yo
+            
         }
     }
 }
