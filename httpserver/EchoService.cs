3f9dab383510aead155b36490a4be960ececad14
MadsMorsing <madsmorsing@gmail.com>
2014-09-29 14:14:27 +0200

    Added EchoService.cs

---
+++ httpserver/EchoService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpserver
+{
+    
+    public class EchoService
+    {
+        private TcpClient connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;
+        }
+
+        public void doIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+            ns.Close();
+            connectionSocket.Close();
+            //serverSocket.Stop();
+        }
+
+    }
+}
