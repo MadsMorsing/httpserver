86e02c9d4718f120ce6cfde06656cdd20fb67c42
MadsMorsing <madsmorsing@gmail.com>
2014-09-29 14:10:55 +0200

    Starten til ny projekt

--- httpserver/Program.cs
+++ httpserver/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace httpserver
@@ -12,7 +14,44 @@ namespace httpserver
         static void Main(string[] args)
         {
             Console.WriteLine("Hello http server");
-            
+
+            {
+                try
+                {
+                    TcpListener serverSocket = new TcpListener(65080);
+                    serverSocket.Start();
+
+
+                    while (true)
+                    {
+                        TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                        //Socket connectionSocket = serverSocket.AcceptSocket();
+                        Console.WriteLine("Server activated");
+
+                        //Stream ns = connectionSocket.GetStream();
+                        //Stream ns = new NetworkStream(connectionSocket);
+                        //StreamReader sr = new StreamReader(ns);
+                        //StreamWriter sw = new StreamWriter(ns);
+                        //sw.AutoFlush = true; // enable automatic flushing
+
+                        EchoService es = new EchoService(connectionSocket);
+
+                        Thread t = new Thread(es.doIt);
+                        t.Start();
+                        //es.doIt();
+
+                    }
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+
+            }
+
         }
+
+
     }
 }
